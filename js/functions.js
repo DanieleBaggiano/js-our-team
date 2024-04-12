dfc94a297c293ff301b97f8e596390d9467b4c8c
DanieleBaggiano <151195732+DanieleBaggiano@users.noreply.github.com>
2024-04-12 16:57:04 +0200

    fine team

--- js/functions.js
+++ js/functions.js
@@ -1,60 +1,34 @@
-const array = [
-    {
-        name: "Wayne Barnett",
-        role: "Founder & CEO",
-        image: "./img/wayne-barnett-founder-ceo.jpg"
-    },
+// const paragrafo = document.createElement('p');
+// paragrafo.textContent = "Founder & CEO";
+// paragrafo.classList.add('card-text');
 
-    {
-        name: "Angela Caroll",
-        role: "Chief Editor",
-        image: "./img/angela-caroll-chief-editor.jpg"
-    },
+// const cardBody = document.querySelector(".card-body");
 
-    {
-        name: "Walter Gordon",
-        role: "Office Manager",
-        image: "./img/walter-gordon-office-manager.jpg"
-    },
+// cardBody.appendChild(paragrafo);
+function team(Print) {
+    const row = document.querySelector(".row")
+    for (let i = 0; i < Print.length; i++) {
+        const curArray = Print[i];
 
-    {
-        name: "Angela Lopez",
-        role: "Social Media Manager",
-        image: "./img/angela-lopez-social-media-manager.jpg"
-    },
+        console.log("name", curArray.name);
+        console.log("role", curArray.role);
+        console.log("image", curArray.image);
 
-    {
-        name: "Scott Estrada",
-        role: "Developer",
-        image: "./img/scott-estrada-developer.jpg"
-    },
+        const divCol = document.createElement("div");
 
-    {
-        name: "Barbara Ramos",
-        role: "Graphic Designer",
-        image: "./img/barbara-ramos-graphic-designer.jpg"
+        divCol.classList.add("col");
+
+        divCol.innerHTML = `
+        <div class="card">
+            <img src="${curArray.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${curArray.name}</h5>
+                    <p class="card-text">${curArray.role}</p>
+                </div>
+        </div>
+    `;
+        console.log(divCol);
+
+        row.append(divCol);
     }
-];
-
-const $one = document.getElementById.bind(document);
-
-const cont = $one("container");
-console.log(cont);
-
-for (let i = 0; i < array.length; i++) {
-    const curArray = array[i];
-
-    console.log(`${curArray.name} ${curArray.role} ${curArray.image}`);
-
-    // for (let key in curArray) {
-
-    // }
 }
-
-const paragrafo = document.createElement('p');
-paragrafo.textContent = "Founder & CEO";
-paragrafo.classList.add('card-text');
-
-const cardBody = document.querySelector(".card-body");
-
-cardBody.appendChild(paragrafo);
